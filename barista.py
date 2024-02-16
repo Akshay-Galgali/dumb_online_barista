aea13126d87aeb4c08d2548c06d84d53fc42edfe
AkshayGalgali <60934796+Akshay-Galgali@users.noreply.github.com>
2024-02-15 23:05:05 -0600

    Create barista.py

---
+++ barista.py
@@ -0,0 +1,4 @@
+name = input("Hello, Welcome to our Barista, May I please have your name?")
+menu= ("Black Coffee, Mocca, and Avacado Toast")
+order = input("Hello " +name + " Today, we are serving " + menu +" What would you like to order?")
+print("Great choice, " + name + " we will have that " + order + " ready for you in a moment")
